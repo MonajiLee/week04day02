58c4f99343f376191113a5fe0e2fa9e2d671e96b
MonajiLee <lee.smonica@gmail.com>
2018-10-16 19:41:36 +0000

    Refactor to seperate data access and input/output function

--- lookup_people.js
+++ lookup_people.js
@@ -1,3 +1,5 @@
+// MIDDLEWARE & SETUP ------------------------------------------
+
 const pg = require("pg");
 const settings = require("./settings");
 
@@ -10,24 +12,31 @@ const client = new pg.Client ({
     ssl      : settings.ssl
 });
 
+// CALLBACK FUNCTION ----------------------------------------------
+
+function printValue(arr){
+  arr.forEach(function(input, i){
+    console.log(`- ${i + 1}: ${input.first_name} ${input.last_nae}, born ${input.birthdate.toISOString().split('T')[0]}`);
+  });
+}
 
 // CONNECT/QUERY/DISCONNECT FROM DATABASE ------------------------
 
-// connect with async/await
 client.connect((err) => {
     if (err) {
       return console.error("Connection Error", err);
     }
 
-    // query with async/await
-            // text (parameterized query), optional values, callback
     client.query(`SELECT * FROM famous_people WHERE first_name LIKE $1`, [process.argv[2]], (err, result) => {
       if (err) {
         return console.error("error running query", err);
       }
-      console.log(result.rows[0])
-      
-      // disconnect with async/await
+
+      // Format of Database Response Query Experience
+      console.log('Searching ...');
+      console.log(`Found ${result.rows.length} person(s) by the name '${process.argv[2]}'`);
+      printValue(result.rows);
+
       client.end();
     });
   });
