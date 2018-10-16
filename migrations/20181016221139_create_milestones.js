9b44f19af12b4390831672cfb8abaadc1f405377
MonajiLee <lee.smonica@gmail.com>
2018-10-16 22:53:10 +0000

    Initial commit

---
+++ migrations/20181016221139_create_milestones.js
@@ -0,0 +1,9 @@
+// called when the migration is applied
+exports.up = function(knex, Promise) {
+
+};
+
+// called on a migration rollback ('undo' the up function)
+exports.down = function(knex, Promise) {
+  
+};
