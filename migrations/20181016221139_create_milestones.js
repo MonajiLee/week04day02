815f2161c6321c9ade6148f54ea865a57a335624
MonajiLee <lee.smonica@gmail.com>
2018-10-16 22:55:24 +0000

    Migrate up/down functions create/drop new milestones table

--- migrations/20181016221139_create_milestones.js
+++ migrations/20181016221139_create_milestones.js
@@ -1,9 +1,13 @@
 // called when the migration is applied
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('milestones', function(table) {
+        table.integer('id');  
+        table.string('description');
+        table.date('date_achieved');
+      })
 };
 
 // called on a migration rollback ('undo' the up function)
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('milestones')
 };
